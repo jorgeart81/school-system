8a6bf8875af00764acda481fc95b75ceb762e4d2
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2025-03-01 00:05:07 -0500

    feat: add identity models

---
+++ Infrastructure/Identity/Models/ApplicationRoleClaim.cs
@@ -0,0 +1,10 @@
+using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.Identity.Models;
+
+public class ApplicationRoleClaim : IdentityRoleClaim<string>
+{
+    public string? Description { get; set; }
+    public string? Group { get; set; }
+}
