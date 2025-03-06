3ccb90304686f09568da09a87b5c39fe52fd4d47
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2025-03-06 18:03:06 -0500

    feat(application): add JwtSettings.cs

---
+++ Applicaction/JwtSettings.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Applicaction;
+
+public class JwtSettings
+{
+    public required string Secret { get; set; }
+    public int TokenExpiryTimeInMinutes { get; set; }
+    public int RefreshTokenExpiryTimeInDays { get; set; }
+}
