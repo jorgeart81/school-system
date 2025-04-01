a86ba6a20dd8c3c0b3cbe9d3dbccef1ec58c35a2
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2025-04-01 18:05:52 -0500

    fix UnauthorizedException

--- Applicaction/Exceptions/UnauthorizedException.cs
+++ Applicaction/Exceptions/UnauthorizedException.cs
@@ -3,8 +3,8 @@ using System.Net;
 
 namespace Applicaction.Exceptions;
 
-public class UnauthorizedException(string message, List<string>? errorMessages = default,
-    HttpStatusCode statusCode = HttpStatusCode.Unauthorized) : Exception(message)
+public class UnauthorizedException(List<string>? errorMessages = default,
+    HttpStatusCode statusCode = HttpStatusCode.Unauthorized) : Exception
 {
     public List<string>? ErrorMessages { get; set; } = errorMessages;
     public HttpStatusCode StatusCode { get; set; } = statusCode;
